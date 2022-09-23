3eff8bc95765b99231637d17b0ecd4b00bc0ca31
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-09-23 12:33:22 -0300

    Change the fileName to fail

--- RevitAddin.CountIt.Test/CountItDocumentTest.cs
+++ RevitAddin.CountIt.Test/CountItDocumentTest.cs
@@ -9,7 +9,7 @@ namespace RevitAddin.UnitTest
 {
     public class CountItDocumentTest : IDisposable
     {
-        const string fileName = "CountIt.rvt";
+        const string fileName = "CountIt2.rvt";
 
         private readonly Document document;
 
